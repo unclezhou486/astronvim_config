4a35513ff70d09b68bba28eb44c29d521f5f8c99
unclezhou486 <1543130456@qq.com>
2025-12-19 19:28:18 +0800

    add new

--- init.lua
+++ init.lua
@@ -24,18 +24,48 @@ if not pcall(require, "lazy") then
   vim.cmd.quit()
 end
 
-vim.api.nvim_set_keymap('n', '<C-A-b>', ':CompetiTest run<CR>', { noremap = true, silent = true }) -- 设置快捷键 Ctrl + Alt + B 来输入 :Competitest run
+vim.api.nvim_set_keymap('n','<C-A-c>',':CompetiTest receive persistently<CR>',{noremap = true,silent = true})
+vim.api.nvim_set_keymap('n', '<C-A-b>', ':w<CR>:CompetiTest run<CR>', { noremap = true, silent = true }) -- 设置快捷键 Ctrl + Alt + B 来输入 :Competitest run
 -- 设置快捷键 Ctrl + 回车（<C-CR>） add test
 vim.api.nvim_set_keymap('n', '<C-CR>', ':CompetiTest add_testcase<CR>', { noremap = true, silent = true })
 -- 设置全局的缩进为 4 格
 
-vim.api.nvim_set_keymap('n','<C-b>',':w<CR>:!g++ -g -std=c++20 -Wall % -o test.out<CR>',{noremap = true,silent =true})
+vim.api.nvim_set_keymap('n','<C-b>',':w<CR>:!g++ -g -O0 -Wextra -fsanitize=address -std=c++20 -Wall \'%\' -o test.out<CR>',{noremap = true,silent =true})
 
-vim.o.tabstop = 4        -- 设置制表符为 4 格
-vim.o.shiftwidth = 4     -- 设置缩进量为 4 格
-vim.o.expandtab = true   -- 使用空格替代制表符
-vim.o.softtabstop = 4    -- 设置软制表符为 4 格
+vim.api.nvim_set_keymap('n',"<C-a>",":%y+<CR>",{noremap = true,silent = true} )
 
+local opts = { noremap = true, silent = true }
+
+-- 上移一行（Alt + k / Alt + Up）
+vim.api.nvim_set_keymap("n", "<A-k>", ":m .-2<CR>==", opts)
+vim.api.nvim_set_keymap("v", "<A-k>", ":m '<-2<CR>gv=gv", opts)
+vim.api.nvim_set_keymap("n", "<A-Up>", ":m .-2<CR>==", opts)
+vim.api.nvim_set_keymap("v", "<A-Up>", ":m '<-2<CR>gv=gv", opts)
+
+-- 下移一行（Alt + j / Alt + Down）
+vim.api.nvim_set_keymap("n", "<A-j>", ":m .+1<CR>==", opts)
+vim.api.nvim_set_keymap("v", "<A-j>", ":m '>+1<CR>gv=gv", opts)
+vim.api.nvim_set_keymap("n", "<A-Down>", ":m .+1<CR>==", opts)
+vim.api.nvim_set_keymap("v", "<A-Down>", ":m '>+1<CR>gv=gv", opts)
+
+vim.opt.tabstop = 4        -- 设置制表符为 4 格
+vim.opt.shiftwidth = 4     -- 设置缩进量为 4 格
+vim.opt.expandtab = true   -- 使用空格替代制表符
+vim.opt.softtabstop = 4    -- 设置软制表符为 4 格
+
+vim.g.autoformat = false
+
+
+-- 强制覆盖所有文件类型为 4 空格
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.softtabstop = 4
+    vim.opt_local.expandtab = true
+  end,
+})
 
 require "lazy_setup"
 require "polish"
